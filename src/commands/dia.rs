31e4a5ab0eaf63089ee1839ab36ee83166e81aae
Keval Shah <captainrex493@gmail.com>
2020-05-16 17:35:04 -0700

    makes !dia ignore kathik

--- src/commands/dia.rs
+++ src/commands/dia.rs
@@ -5,7 +5,7 @@ use crate::commands::message_generation;
 
 #[command]
 fn dia(ctx: &mut Context, msg: &Message) -> CommandResult {
-    if msg.author.to_string() != "<@483297157580062732>" {
+    if msg.author.to_string() == "<@207686242874294272>" {
         return Ok(());
     };
     let _ = msg.channel_id.say(&ctx.http, message_generation::get_dia_string());
